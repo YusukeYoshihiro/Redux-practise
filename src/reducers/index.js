dcd3af06e744ca4ff3fd0badd6152d12ea6edee0
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-05-21 23:32:17 -0700

    Changes to be committed: 	new file:   .gitignore 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/actions/index.js 	new file:   src/components/App.js 	new file:   src/index.js 	new file:   src/reducers/index.js 	new file:   yarn.lock

---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+
+
+const songsReducer = () => {
+  return [
+    { title: 'Wondderwall', duration: '4:40' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'I Want it That Way', duration: '1:45' },
+  ];
+};
+
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
